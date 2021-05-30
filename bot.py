846d0db507ca1ecb9297051451f7b9aac862ac65
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-30 17:46:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,12 +13,10 @@ Bot = Client(
         suppchat = os.environ("SUPPORT_CHAT"]
 )
 
->>>>>>>>>>>>>>>>>> MESSAGES <<<<<<<<<<<<<<<<<<<<<<<<<<
-
 START_TEXT = """
 Hai {} , This Bot Is Under Maintenance.
 
-You Can't Use This Bot Right Now.You Will Get a Message On This Bot's Channel If This Bot Is Ready To Work.
+You Can't Use This Bot Right Now.You Will Get a Message On This Bot's Channel If This Bot Is Ready To Work.To Know More Contact Support Group:)
 
 ℹ️ Maintenance Bot.
 """
