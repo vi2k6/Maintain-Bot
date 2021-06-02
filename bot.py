204ebc25afa3c6b4735dad0542e08ee2b5a64d8c
alenpaul2001 <alenpaul2001@gmail.com>
2021-06-02 21:54:38 +0530

    fixed some linting issues

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-Bot = Client(
-        "Maintenance Bot",
-        bot_token = os.environ["BOT_TOKEN"],
-        api_id = int(os.environ["API_ID"]),
-        api_hash = os.environ["API_HASH"]
+bot = Client(
+    "Maintenance Bot",
+    bot_token=os.environ["BOT_TOKEN"],
+    api_id=int(os.environ["API_ID"]),
+    api_hash=os.environ["API_HASH"],
 )
 
-updatesc = os.environ["UPDATES_CHANNEL"],
+updatesc = (os.environ["UPDATES_CHANNEL"],)
 supportc = os.environ["SUPPORT_CHAT"]
 
 BOT_TEXT = """
@@ -21,21 +21,22 @@ You Can't Use This Bot Right Now.You Will Get a Message On This Bot's Channel If
 """
 
 BOT_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton(text="Channel", url=f"https://telegram.me/{updatesc}"),
-        InlineKeyboardButton(text="Support", url=f"https://telegram.me/{supportc}")
-        ]]
-    )
-                
-@Bot.on_message(filters.private & filters.text)
+    [
+        [
+            InlineKeyboardButton(text="Channel", url=f"https://telegram.me/{updatesc}"),
+            InlineKeyboardButton(text="Support", url=f"https://telegram.me/{supportc}"),
+        ]
+    ]
+)
+
+
+@bot.on_message(filters.private & filters.text)
 async def start(bot, update):
     text = BOT_TEXT.format(update.from_user.mention)
     reply_markup = BOT_BUTTONS
     await update.reply_text(
-        text=text,
-        disable_web_page_preview=True,
-        reply_markup=reply_markup
+        text=text, disable_web_page_preview=True, reply_markup=reply_markup
     )
 
 
-Bot.run()
+bot.run()
