3affbb3cbd37e60bf59d3089e5c776e632a4968e
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-07-01 18:25:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,6 @@ async def start(bot, update):
     text = BOT_TEXT.format(update.from_user.mention)
     reply_markup = BOT_BUTTONS
     await message.reply_photo(photo="https://telegra.ph/file/60fdc9e6682702bf4b471.jpg")
-    await update.reply_text(
         text=text, disable_web_page_preview=True, reply_markup=reply_markup
     )
     logger.info(f"{m.from_user.first_name} started The Bot!!!")
