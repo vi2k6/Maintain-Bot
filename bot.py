dbdd57d3b4601157d20855a2324187919f22959e
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-07-01 19:43:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ async def start(client, message):
     text = BOT_TEXT.format(message.from_user.mention)
     reply_markup = BOT_BUTTONS
     await message.reply_photo(photo="https://telegra.ph/file/60fdc9e6682702bf4b471.jpg")
-    await message.reply_text( text=text, disable_web_page_preview=True, reply_markup=reply_markup )
+    await message.reply_text( text=text, disable_web_page_preview=True, reply_markup=reply_markup ).logging.info("Started Your bot.")
 
 # You Can See That Who Is Started The Bot On Logs
 
