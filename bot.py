20e9642fb972f9a4f8cf0bbbeea58a2091beb0a2
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-08-24 08:54:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,12 @@
 """
 Maintain, Telegram Maintain Bot
 
-Copyright (C) 2021  Vivek-TP <https://t.me/Vivek_Kerala>
+Copyright (C) 2021 Vivek-TP <https://t.me/Vivek_Kerala>
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 """
-# Made with Python3
-# (C) Vivek-TP and FayasNoushad
-
 import os
 import logging
 from pyrogram import Client, filters
